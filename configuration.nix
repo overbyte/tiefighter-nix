8ee47492d89f85ea2098ffecc20745056440441e
Allandt Bik-Elliott <allandt@gmail.com>
2025-02-07 17:21:47 +0000

    fix tabbing

--- configuration.nix
+++ configuration.nix
@@ -46,21 +46,21 @@
   
   # Exclude some gnome packages
   environment.gnome.excludePackages = (with pkgs; [
-  atomix # puzzle game
-  cheese # webcam tool
-  epiphany # web browser
-  evince # document viewer
-  geary # email reader
-  gedit # text editor
-  gnome-characters
-  gnome-music
-  gnome-photos
-  gnome-terminal
-  gnome-tour
-  hitori # sudoku game
-  iagno # go game
-  tali # poker game
-  totem # video player
+    atomix # puzzle game
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-characters
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
   ]);
 
 
