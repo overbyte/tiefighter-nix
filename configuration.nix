d771c46bcbbb614876aad71f4691de3006d5b23f
Allandt Bik-Elliott <allandt@gmail.com>
2025-02-07 16:26:52 +0000

    add gnome

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,32 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
+  # Add Gnome - see https://nixos.wiki/wiki/GNOME 
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+  
+  # Exclude some gnome packages
+  environment.gnome.excludePackages = (with pkgs; [
+  atomix # puzzle game
+  cheese # webcam tool
+  epiphany # web browser
+  evince # document viewer
+  geary # email reader
+  gedit # text editor
+  gnome-characters
+  gnome-music
+  gnome-photos
+  gnome-terminal
+  gnome-tour
+  hitori # sudoku game
+  iagno # go game
+  tali # poker game
+  totem # video player
+  ]);
+
 
   
 
