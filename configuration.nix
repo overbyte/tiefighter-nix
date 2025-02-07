412247a407fbedb87f348466abc65550bc2bc62e
Allandt Bik-Elliott <allandt@gmail.com>
2025-02-07 17:22:01 +0000

    add xserver.xkb options

--- configuration.nix
+++ configuration.nix
@@ -63,12 +63,9 @@
     totem # video player
   ]);
 
-
-  
-
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
